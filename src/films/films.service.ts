933da0abd8c1ee15fa3566c9cdf0d8a442d99bd8
cloudaxhub <126250072+ebenezerugocloudax@users.noreply.github.com>
2023-09-05 13:10:48 +0100

    chore(log): add logs fix issue

--- src/films/films.service.ts
+++ src/films/films.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreateFilmDto } from './dto/create-film.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Film } from './entities/film.entity';
@@ -7,63 +7,164 @@ import { DeepPartial, Repository } from 'typeorm';
 import { IPaginationOptions } from '../utils/types/pagination-options';
 import { EntityCondition } from '../utils/types/entity-condition.type';
 import { NullableType } from '../utils/types/nullable.type';
-import { AddCommentDto } from "./dto/add-comment.dto";
+import { AddCommentDto } from './dto/add-comment.dto';
 
 @Injectable()
 export class FilmsService {
+  private readonly logger = new Logger(FilmsService.name);
   constructor(
     @InjectRepository(Film) private readonly filmRepository: Repository<Film>,
     @InjectRepository(Comment)
     private readonly commentRepository: Repository<Comment>,
   ) {}
   create(createFilmDto: CreateFilmDto) {
-    const film = this.filmRepository.create(createFilmDto);
-    return this.filmRepository.save(film);
+    try {
+      this.logger.log('Creating new film');
+      const film = this.filmRepository.create(createFilmDto);
+      return this.filmRepository.save(film);
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          errors: {
+            body: error.message,
+          },
+          message: 'Error occurred',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   findAll(paginationOptions: IPaginationOptions): Promise<Film[]> {
-    return this.filmRepository
-      .createQueryBuilder('film')
-      .loadRelationCountAndMap('film.numberOfComments', 'film.comments')
-      .skip((paginationOptions.page - 1) * paginationOptions.limit)
-      .take(paginationOptions.limit)
-      .orderBy('film.releaseDate', 'ASC')
-      .getMany();
-    // return this.filmRepository.find({
-    //   skip: (paginationOptions.page - 1) * paginationOptions.limit,
-    //   take: paginationOptions.limit,
-    //   order: { releaseDate: 'ASC' },
-    // });
+    try {
+      this.logger.log('Finding all films');
+      return this.filmRepository
+        .createQueryBuilder('film')
+        .loadRelationCountAndMap('film.numberOfComments', 'film.comments')
+        .skip((paginationOptions.page - 1) * paginationOptions.limit)
+        .take(paginationOptions.limit)
+        .orderBy('film.releaseDate', 'ASC')
+        .getMany();
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          errors: {
+            body: error.message,
+          },
+          message: 'Error occurred',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   findOne(fields: EntityCondition<Film>): Promise<NullableType<Film>> {
-    return this.filmRepository.findOne({
-      where: fields,
-      relations: ['comments'],
-    });
+    try {
+      this.logger.log('Finding film');
+      return this.filmRepository.findOne({
+        where: fields,
+        relations: ['comments'],
+      });
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          errors: {
+            body: error.message,
+          },
+          message: 'Error occurred',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   update(id: string, payload: DeepPartial<Film>) {
-    return this.filmRepository.update(id, payload);
+    try {
+      this.logger.log('Updating film');
+      return this.filmRepository.update(id, payload);
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          errors: {
+            body: error.message,
+          },
+          message: 'Error occurred',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   remove(id: string) {
-    return this.filmRepository.softDelete(id);
+    try {
+      this.logger.log('Deleting film');
+      return this.filmRepository.softDelete(id);
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          errors: {
+            body: error.message,
+          },
+          message: 'Error occurred',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async addComment(id: string, payload: AddCommentDto) {
-    const film = await this.filmRepository.findOne({ where: { id } });
-    const comment = this.commentRepository.create(payload);
-    comment.film = film;
-    return this.commentRepository.save(comment);
+    try {
+      this.logger.log('Adding comment to film');
+      const film = await this.filmRepository.findOne({ where: { id } });
+      const comment = this.commentRepository.create(payload);
+      comment.film = film;
+      return this.commentRepository.save(comment);
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          errors: {
+            body: error.message,
+          },
+          message: 'Error occurred',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findAllComments(id: string, paginationOptions: IPaginationOptions) {
-    return this.commentRepository.find({
-      where: { film: { id } },
-      skip: (paginationOptions.page - 1) * paginationOptions.limit,
-      take: paginationOptions.limit,
-      order: { createdAt: 'ASC' },
-    });
+    try {
+      this.logger.log('Finding all comments');
+      return this.commentRepository.find({
+        where: { film: { id } },
+        skip: (paginationOptions.page - 1) * paginationOptions.limit,
+        take: paginationOptions.limit,
+        order: { createdAt: 'ASC' },
+      });
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          errors: {
+            body: error.message,
+          },
+          message: 'Error occurred',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
   controllers: [HealthController],
-  providers: [HealthService],
   imports: [TerminusModule],
 })
 export class HealthModule {}
